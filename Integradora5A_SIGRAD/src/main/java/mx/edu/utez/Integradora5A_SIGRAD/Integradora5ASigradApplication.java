6febd72d8bde6a2325f22b44f92555181d29b1ff
Jorge-Moranchel <20243ds059@utez.edu.mx>
2026-03-06 12:00:54 -0600

    Hice el archivo de spring para que creemos las carpetas de donde va a ir el backend y el frontend

---
+++ Integradora5A_SIGRAD/src/main/java/mx/edu/utez/Integradora5A_SIGRAD/Integradora5ASigradApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.Integradora5A_SIGRAD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Integradora5ASigradApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Integradora5ASigradApplication.class, args);
+	}
+
+}
---
+++ Integradora5A_SIGRAD/src/test/java/mx/edu/utez/Integradora5A_SIGRAD/Integradora5ASigradApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.Integradora5A_SIGRAD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Integradora5ASigradApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
