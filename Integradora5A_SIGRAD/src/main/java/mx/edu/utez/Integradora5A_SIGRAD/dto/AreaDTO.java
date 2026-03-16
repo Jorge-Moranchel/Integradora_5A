f71d04d256c3e7c65b5a6b590aad5fc8abbff016
OsvaldoMezaHerrea <20243ds051@utez.edu.mx>
2026-03-15 22:29:20 -0600

    Crud de areas terminado Modulo 1 front y back OsvaldoMezaDev

---
+++ Integradora5A_SIGRAD/src/main/java/mx/edu/utez/Integradora5A_SIGRAD/dto/AreaDTO.java
@@ -0,0 +1,55 @@
+package mx.edu.utez.Integradora5A_SIGRAD.dto;
+
+public class AreaDTO {
+
+    private String nombre;
+    private String ubicacion;
+    private String horaApertura;
+    private String horaCierre;
+    private String imagen;
+
+    // Constructor vacío
+    public AreaDTO() {
+    }
+
+    // Getters y Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public String getHoraApertura() {
+        return horaApertura;
+    }
+
+    public void setHoraApertura(String horaApertura) {
+        this.horaApertura = horaApertura;
+    }
+
+    public String getHoraCierre() {
+        return horaCierre;
+    }
+
+    public void setHoraCierre(String horaCierre) {
+        this.horaCierre = horaCierre;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+}
