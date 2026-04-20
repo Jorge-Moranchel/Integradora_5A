06f8360c83f931d172a5e7a90bb29e0e5f7b5ac9
Jorge-Moranchel <20243ds059@utez.edu.mx>
2026-04-20 09:31:02 -0600

    Ya hice cambios en UsuarioRepository sobre el codigo de verificacion del usuario

--- Integradora5A_SIGRAD/src/main/java/mx/edu/utez/Integradora5A_SIGRAD/repository/UsuarioRepository.java
+++ Integradora5A_SIGRAD/src/main/java/mx/edu/utez/Integradora5A_SIGRAD/repository/UsuarioRepository.java
@@ -2,11 +2,14 @@ package mx.edu.utez.Integradora5A_SIGRAD.repository;
 
 import mx.edu.utez.Integradora5A_SIGRAD.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByEmailInstitucional(String email);
     boolean existsByEmailInstitucional(String email);
-
     Optional<Usuario> findByCodigoVerificacion(String codigoVerificacion);
+
+    List<Usuario> findByValidado(Boolean validado);
 }
